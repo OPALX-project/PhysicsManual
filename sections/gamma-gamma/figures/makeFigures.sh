5309bf6b76dfb6bc8c7ec4a1a09e1b036c50d6e3
Andreas Adelmann <andreas.adelmann@psi.ch>
2026-04-25 23:24:43 +0200

    reshufeling

--- sections/gamma-gamma/figures/makeFigures.sh
+++ sections/gamma-gamma/figures/makeFigures.sh
@@ -1,2 +1,2 @@
-pdflatex ics_90deg_figure.tex && cp ics_90deg_figure.pdf ..
-pdflatex breit_wheeler_qed_style_tikz.tex && cp breit_wheeler_qed_style_tikz.pdf ..
+pdflatex ics_90deg_figure.tex
+pdflatex breit_wheeler_qed_style_tikz.tex
